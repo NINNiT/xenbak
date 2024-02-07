5beb27f44bd1d5663cd7695075b802b1e33b3cdc
Simon Sölder <soelder@trafficon.eu>
2024-02-07 13:08:26 +0100

    feat: adapt config

--- copy_to_dev.sh
+++ copy_to_dev.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+
 scp target/x86_64-unknown-linux-musl/release/xenbakd root@192.168.100.2:/tmp
-scp config.toml root@192.168.100.2:/tmp
+scp apps/xenbakd/config.toml root@${XEN_HOST}:/tmp
